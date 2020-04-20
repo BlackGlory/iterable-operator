94f7de47a73086ca8f79d94b865a71bda5e59b67
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-04-20 17:29:45 +0800

    v0.8.2
    Fix consume signatures

--- src/style/chaining/tail/consume.ts
+++ src/style/chaining/tail/consume.ts
@@ -3,7 +3,7 @@ import { consume as target } from '@tail/consume'
 import { IterableOperatorBase } from '../iterable-operator-base'
 
 export class ConsumeOperator<T> extends IterableOperatorBase<T> {
-  consume<U>(consumer: U): U
+  consume<U>(consumer: (iterable: Iterable<T>) => U): U
   consume(...args: unknown[]) {
     return applyBinding(this, target, args)
   }
