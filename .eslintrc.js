2ec51d94fbbd6ba96d2d76c472b9cf4484c79342
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-24 03:06:27 +0800

    chore: upgrade dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
   , 'no-empty': 'off'
   , '@typescript-eslint/no-inferrable-types': 'off'
   , '@typescript-eslint/ban-types': 'off'
+  , '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
