0c864c0277655957d3259b23f57ac0ae3ec45c0d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:42:14 +0000

    refactor: simplify

--- src/every-async.ts
+++ src/every-async.ts
@@ -1,31 +1,7 @@
 import { Awaitable} from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function everyAsync<T>(
+export async function everyAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<boolean> {
-  if (isAsyncIterable(iterable)) {
-    return everyAsyncIterable(iterable, predicate)
-  } else {
-    return everyIterable(iterable, predicate)
-  }
-}
-
-async function everyIterable<T>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<boolean> {
-  let index = 0
-  for (const element of iterable) {
-    if (!await predicate(element, index)) return false
-    index++
-  }
-  return true
-}
-
-async function everyAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): Promise<boolean> {
   let index = 0
