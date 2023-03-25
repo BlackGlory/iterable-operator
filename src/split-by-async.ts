42ce9e7ff6557510110815d33c219d762d08fecb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:48:39 +0000

    refactor: simplify

--- src/split-by-async.ts
+++ src/split-by-async.ts
@@ -1,39 +1,7 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function splitByAsync<T>(
+export async function* splitByAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T[]> {
-  if (isAsyncIterable(iterable)) {
-    return splitByAsyncIterable(iterable, predicate)
-  } else {
-    return splitByIterable(iterable, predicate)
-  }
-}
-
-async function* splitByIterable<T>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T[]> {
-  let buffer: T[] = []
-  let index = 0
-
-  for (const element of iterable) {
-    if (await predicate(element, index)) {
-      yield buffer
-      buffer = []
-    } else {
-      buffer.push(element)
-    }
-    index++
-  }
-
-  yield buffer
-}
-
-async function* splitByAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): AsyncIterableIterator<T[]> {
   let buffer: T[] = []
--- src/take-until-async.ts
+++ src/take-until-async.ts
@@ -1,37 +1,12 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function takeUntilAsync<T>(
+export async function* takeUntilAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T> {
-  if (isAsyncIterable(iterable)) {
-    return takeUntilAsyncIterable(iterable, predicate)
-  } else {
-    return takeUntilIterable(iterable, predicate)
-  }
-}
-
-async function* takeUntilAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): AsyncIterableIterator<T> {
   let index = 0
 
   for await (const element of iterable) {
-    if (await predicate(element, index)) break
-    yield element
-    index++
-  }
-}
-
-async function* takeUntilIterable<T>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T> {
-  let index = 0
-
-  for (const element of iterable) {
     if (await predicate(element, index)) break
     yield element
     index++
