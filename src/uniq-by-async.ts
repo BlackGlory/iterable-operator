4c8d3b1b51f2a61a5415a98b31cd2bdc72a46bc6
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:55:11 +0000

    refactor: simplify

--- src/uniq-by-async.ts
+++ src/uniq-by-async.ts
@@ -1,40 +1,12 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function uniqByAsync<T, U>(
+export async function* uniqByAsync<T, U>(
   iterable: Iterable<T> | AsyncIterable<T>
-, fn: (element: T, index: number) => Awaitable<U>
-): AsyncIterableIterator<T> {
-  if (isAsyncIterable(iterable)) {
-    return uniqByAsyncIterable(iterable, fn)
-  } else {
-    return uniqByIterable(iterable, fn)
-  }
-}
-
-async function* uniqByAsyncIterable<T, U>(
-  iterable: AsyncIterable<T>
 , fn: (element: T, index: number) => Awaitable<U>
 ): AsyncIterableIterator<T> {
   const bucket = new Set<U>()
   let index = 0
   for await (const element of iterable) {
-    const result = await fn(element, index)
-    if (!bucket.has(result)) {
-      yield element
-      bucket.add(result)
-    }
-    index++
-  }
-}
-
-async function* uniqByIterable<T, U>(
-  iterable: Iterable<T>
-, fn: (element: T, index: number) => Awaitable<U>
-): AsyncIterableIterator<T> {
-  const bucket = new Set<U>()
-  let index = 0
-  for (const element of iterable) {
     const result = await fn(element, index)
     if (!bucket.has(result)) {
       yield element
