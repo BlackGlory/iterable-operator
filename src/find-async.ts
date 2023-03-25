a3afd530f4398e559600877fa0fa39305d2a02ce
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:43:53 +0000

    refactor: simplify

--- src/find-async.ts
+++ src/find-async.ts
@@ -1,33 +1,7 @@
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 import { Awaitable } from 'justypes'
 
-export function findAsync<T>(
+export async function findAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<T | undefined> {
-  if (isAsyncIterable(iterable)) {
-    return findAsyncIterable(iterable, predicate)
-  } else {
-    return findIterable(iterable, predicate)
-  }
-}
-
-async function findIterable<T>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<T | undefined> {
-  let index = 0
-
-  for (const element of iterable) {
-    if (await predicate(element, index)) return element
-    index++
-  }
-
-  return undefined
-}
-
-async function findAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): Promise<T | undefined> {
   let index = 0
@@ -38,4 +12,5 @@ async function findAsyncIterable<T>(
   }
 
   return undefined
+
 }
