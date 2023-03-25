d8ae5513a5e77a7af8dcda5cc0eedc91f156ebb7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:47:36 +0000

    refactor: simplify

--- src/some-async.ts
+++ src/some-async.ts
@@ -1,33 +1,7 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function someAsync<T>(
+export async function someAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<boolean> {
-  if (isAsyncIterable(iterable)) {
-    return someAsyncIterable(iterable, predicate)
-  } else {
-    return someIterable(iterable, predicate)
-  }
-}
-
-async function someIterable<T>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): Promise<boolean> {
-  let index = 0
-
-  for (const element of iterable) {
-    if (await predicate(element, index)) return true
-    index++
-  }
-
-  return false
-}
-
-async function someAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): Promise<boolean> {
   let index = 0
