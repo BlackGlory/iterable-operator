10a5599f4b2a2ac056b6dea657e6ae2a5c595f0c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:43:04 +0000

    refactor: simplify

--- src/filter-async.ts
+++ src/filter-async.ts
@@ -1,34 +1,11 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function filterAsync<T, U extends T = T>(
+export async function* filterAsync<T, U extends T = T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<U> {
-  if (isAsyncIterable(iterable)) {
-    return filterAsyncIterable(iterable, predicate)
-  } else {
-    return filterIterable(iterable, predicate)
-  }
-}
-
-async function* filterAsyncIterable<T, U>(
-  iterable: AsyncIterable<T>
 , predicate: (element: T, index: number) => Awaitable<unknown>
 ): AsyncIterableIterator<U> {
   let index = 0
   for await (const element of iterable) {
-    if (await predicate(element, index)) yield element as unknown as U
-    index++
-  }
-}
-
-async function* filterIterable<T, U>(
-  iterable: Iterable<T>
-, predicate: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<U> {
-  let index = 0
-  for (const element of iterable) {
     if (await predicate(element, index)) yield element as unknown as U
     index++
   }
