0ef5d9411695ab3beecfe228fd38f9c5e33ea5d0
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:53:43 +0000

    refactor: simplify

--- src/tap-async.ts
+++ src/tap-async.ts
@@ -1,31 +1,7 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function tapAsync<T>(
+export async function* tapAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, fn: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T> {
-  if (isAsyncIterable(iterable)) {
-    return tapAsyncIterable(iterable, fn)
-  } else {
-    return tapIterable(iterable, fn)
-  }
-}
-
-async function* tapIterable<T>(
-  iterable: Iterable<T>
-, fn: (element: T, index: number) => Awaitable<unknown>
-): AsyncIterableIterator<T> {
-  let index = 0
-  for (const element of iterable) {
-    await fn(element, index)
-    yield element
-    index++
-  }
-}
-
-async function* tapAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , fn: (element: T, index: number) => Awaitable<unknown>
 ): AsyncIterableIterator<T> {
   let index = 0
