7cd5ed00d436e3ffbb2c687c6e92e9ba6e0bc34e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:45:27 +0000

    refactor: simplify

--- src/map-async.ts
+++ src/map-async.ts
@@ -1,34 +1,11 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function mapAsync<T, U>(
+export async function* mapAsync<T, U>(
   iterable: Iterable<T> | AsyncIterable<T>
-, fn: (element: T, index: number) => Awaitable<U>
-): AsyncIterableIterator<U> {
-  if (isAsyncIterable(iterable)) {
-    return mapAsyncIterable(iterable, fn)
-  } else {
-    return mapIterable(iterable, fn)
-  }
-}
-
-async function* mapAsyncIterable<T, U>(
-  iterable: AsyncIterable<T>
 , fn: (element: T, index: number) => Awaitable<U>
 ): AsyncIterableIterator<U> {
   let index = 0
   for await (const element of iterable) {
-    yield await fn(element, index)
-    index++
-  }
-}
-
-async function* mapIterable<T, U>(
-  iterable: Iterable<T>
-, fn: (element: T, index: number) => Awaitable<U>
-): AsyncIterableIterator<U> {
-  let index = 0
-  for (const element of iterable) {
     yield await fn(element, index)
     index++
   }
