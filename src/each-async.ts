921b31beaa266332018bfa480b433af457167cf2
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-25 17:41:06 +0000

    refactor: simplify

--- src/each-async.ts
+++ src/each-async.ts
@@ -1,19 +1,7 @@
 import { Awaitable } from 'justypes'
-import { isAsyncIterable } from '@src/is-async-iterable.js'
 
-export function eachAsync<T>(
+export async function eachAsync<T>(
   iterable: Iterable<T> | AsyncIterable<T>
-, fn: (element: T, index: number) => Awaitable<unknown>
-): Promise<void> {
-  if (isAsyncIterable(iterable)) {
-    return eachAsyncIterable(iterable, fn)
-  } else {
-    return eachIterable(iterable, fn)
-  }
-}
-
-async function eachAsyncIterable<T>(
-  iterable: AsyncIterable<T>
 , fn: (element: T, index: number) => Awaitable<unknown>
 ): Promise<void> {
   let index = 0
@@ -22,16 +10,5 @@ async function eachAsyncIterable<T>(
     await fn(element, index)
     index++
   }
-}
 
-async function eachIterable<T>(
-  iterable: Iterable<T>
-, fn: (element: T, index: number) => Awaitable<unknown>
-): Promise<void> {
-  let index = 0
-
-  for (const element of iterable) {
-    await fn(element, index)
-    index++
-  }
 }
